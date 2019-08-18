a55ee6a91c435efdda2a326eb2bd5830bd34a78f
Rob L <viewpl@gmail.com>
2019-08-18 23:08:54 +1000

    add search component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Search from './Search';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <div className="container">
+        <Search />
+      </div>
+    </div>
+  );
 }
 
 export default App;
