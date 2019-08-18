3c070092d5dda5bebed5286fe99d3cd323532dc2
Rob L <viewpl@gmail.com>
2019-08-19 08:03:41 +1000

    Updates

---
+++ precache-manifest.e1fdec74f7c0aa614218fb5a136011d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8567942b0471ce9e6585edf63057e686",
+    "url": "/elevio-rest/index.html"
+  },
+  {
+    "revision": "9d1c05ad4a91a021c0dd",
+    "url": "/elevio-rest/static/css/2.4c0f34c0.chunk.css"
+  },
+  {
+    "revision": "a0ff86159ac02938ff5f",
+    "url": "/elevio-rest/static/css/main.b363755c.chunk.css"
+  },
+  {
+    "revision": "9d1c05ad4a91a021c0dd",
+    "url": "/elevio-rest/static/js/2.785f6383.chunk.js"
+  },
+  {
+    "revision": "a0ff86159ac02938ff5f",
+    "url": "/elevio-rest/static/js/main.ce934f97.chunk.js"
+  },
+  {
+    "revision": "0648188b7651d2845dc5",
+    "url": "/elevio-rest/static/js/runtime~main.4ac09f22.js"
+  }
+]);
